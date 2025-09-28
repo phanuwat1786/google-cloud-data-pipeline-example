d03a0ca9db4c48c5e89684709c66a859af79eded
phanuwat.su <phanuwat_su@inet.co.th>
2025-09-28 15:55:16 +0700

    initial commit

---
+++ thailand_crop_price_dag.py
@@ -0,0 +1,31 @@
+from airflow.decorators import task
+from airflow import DAG
+from airflow.models import Variable
+import pandas as pd
+
+with DAG(
+    dag_id='thailand_crop_price',
+    start_date=None,
+    catchup=False
+) as dag:
+
+    @task()
+    def save_data_from_kaggle_to_storage():
+        import kagglehub
+        from kagglehub import KaggleDatasetAdapter
+        import os
+        
+        os.environ['KAGGLE_USERNAME'] = Variable.get("kaggle_user")
+        os.environ['KAGGLE_KEY'] = Variable.get("kaggle_key")
+        
+        df = kagglehub.dataset_load(
+            KaggleDatasetAdapter.PANDAS,
+            "unitednations/global-food-agriculture-statistics",
+            "current_FAO/raw_files/Prices_E_All_Data_(Normalized).csv",
+            pandas_kwargs={"encoding": "latin-1"},
+        )
+
+        df.to_parquet('home/airflow/gcs/data/thailand_crop_price.aprquet',index = False)
+
+    t1 = save_data_from_kaggle_to_storage()
+    t1
